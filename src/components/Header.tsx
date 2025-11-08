0a36e039293529ace58cb4dbccf78219f616531b
daffadrm <daffarayhan18@gmail.com>
2025-11-08 20:41:46 +0700

    fix: handle clear selected service

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,13 @@
+import { useDispatch } from "react-redux";
 import { Link, NavLink } from "react-router-dom";
 
 import Logo from "@/assets/images/Logo.png";
+import { setSelectedService } from "@/features/information/infoSlice";
+import { AppDispatch } from "@/store/store";
 
 export default function Header() {
+  const dispatch = useDispatch<AppDispatch>();
+
   const menuItems = [
     { name: "Top Up", path: "/topup" },
     { name: "Transaction", path: "/transaction" },
@@ -12,7 +17,11 @@ export default function Header() {
   return (
     <header className=" border-b bg-white border-gray-200 fixed top-0 left-0 w-full z-50">
       <div className="w-full px-[10vw] flex justify-between items-center py-3">
-        <Link to="/" className="flex items-center space-x-2">
+        <Link
+          to="/"
+          className="flex items-center space-x-2"
+          onClick={() => dispatch(setSelectedService(null))}
+        >
           <img src={Logo} alt="Logo" className="w-8 h-8" />
           <span className="text-lg font-semibold text-black">SIMS PPOB</span>
         </Link>
