b25fc5843bb2852e65bff385bec2abd1824a8505
daffadrm <daffarayhan18@gmail.com>
2025-11-08 19:25:23 +0700

    fix: config vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  build: {
+    outDir: "dist",
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
